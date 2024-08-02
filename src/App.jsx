2a5f96225522e66029a1e1eefd16ab6929857e21
Sankhadip Roy <sankhadiproy23@gmail.com>
2024-08-03 03:20:16 +0530

    ui update
    image preview, headers, borders added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,20 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [file, setFile] = useState(null);
+  const [imageSrc, setImageSrc] = useState('');
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    // create a URL for the image and set it as the source for the image
+    if (selectedFile) {
+      const imageUrl = URL.createObjectURL(selectedFile);
+      setImageSrc(imageUrl);
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -25,20 +35,35 @@ function App() {
       console.log('File uploaded successfully:', response.data, response); // for testing
 
     } catch (error) {
-      console.error('Error uploading file:', error);
+      setError(error);
+      console.error('Error uploading file:', error); // for testing
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <>
       <div>
-        <form onSubmit={handleSubmit}>
-          <input name='file' type="file" onChange={handleFileChange} />
-          <button type="submit">Upload</button>
-        </form>
+        <div style={{ border: '1px solid grey' }}>
+          <h4>Upload the photo of plant leaf: </h4>
+          <div style={{ border: '1px solid grey', padding: '4px' }}>
+            <form onSubmit={handleSubmit}>
+              <input name='file' type="file" onChange={handleFileChange} />
+              <button type="submit">Upload</button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <h4>Uploaded Image Preview: </h4>
+          {imageSrc && <img src={imageSrc} alt="Selected" style={{ width: '100%' }} />}
+        </div>
       </div>
-      <div>
-        <p>disease: {JSON.stringify(data, null, 2)}</p>
+      <div style={{ border: '1px solid grey' }}>
+        {
+          loading ? <p>Disease detection: Loading...</p> :
+            error ? <p>Error: {error.message}</p> : <p>Disease detected : {JSON.stringify(data, null, 2)}</p>
+        }
       </div>
     </>
   )
